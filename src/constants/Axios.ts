c4ea207c06cb63e2a8179562bb21027e3af25de9
syfulislam763 <mahimkhan763@gmail.com>
2026-02-19 04:00:45 +0600

    refactored axios file

--- src/constants/Axios.ts
+++ src/constants/Axios.ts
@@ -6,12 +6,6 @@ import { BASE_URL } from './Path';
 
 const api = axios.create({baseURL: BASE_URL});
 
-interface Payload {
-    accessToken?: string | null
-    refreshToken?: string | null
-}
-
-
 export const setHeaderToken = (token:string) => {
   api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
@@ -19,38 +13,6 @@ export const setHeaderToken = (token:string) => {
 export const deleteHeaderToken = () => {
   delete api.defaults.headers.common['Authorization'];
 }
-export const loadAuthToken = async (cb:(item:Payload) => void) => {
-  const accessToken = await AsyncStorage.getItem('accessToken');
-  const refreshToken = await AsyncStorage.getItem('refreshToken');
-
-  if (accessToken) {
-    api.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-  }
-  cb({
-    accessToken,
-    refreshToken,
-  });
-};
-
-
-export const setAuthToken = async (accessToken:string, refreshToken:string, cb:()=> void) => {
-  await AsyncStorage.setItem('accessToken', accessToken);
-  await AsyncStorage.setItem('refreshToken', refreshToken);
-  api.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-  cb()
-};
-
-export const logoutUser = async (cb:()=>void) => {
-  await AsyncStorage.removeItem('accessToken');
-  await AsyncStorage.removeItem('refreshToken');
-  await AsyncStorage.removeItem('store');
-  await AsyncStorage.clear();
-  delete api.defaults.headers.common['Authorization'];
-
-  cb && cb();
-};
-
-
 
 
 
